a70a094dce2d8d40e48214d8804e4352bae6cd5a
Sacha Marty <sacha.marty@epita.fr>
2022-04-22 15:23:24 +0200

    update

---
+++ assets/js/index.js
@@ -0,0 +1,27 @@
+﻿
+
+/*
+function sendEmail() {
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "opteamisprime@gmail.com",
+        Password: "aaubSLXWdHj3iqE",
+        To: 'opteamisprime@gmail.com',
+        From: "opteamisprime@gmail.com",
+        Subject: "tott",
+        Body: "noel"
+
+        Host: "smtp.gmail.com",
+        Username: "opteamisprime@gmail.com",
+        Password: "aaubSLXWdHj3iqE",
+        To: 'opteamisprime@gmail.com',
+        From: "opteamisprime@gmail.com",
+        Subject: "tott",
+        Body: "noel"
+        
+    }).then(
+        message => alert("mail sent successfully")
+    );
+}
+*/
+
